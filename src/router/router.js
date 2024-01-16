285675acda06b47066f63490ed85ab794ed330fa
mdarmanbd <md.armantext@gmail.com>
2024-01-16 18:36:26 +0600

    remove the unnesseray code

--- src/router/router.js
+++ src/router/router.js
@@ -1,11 +1,18 @@
 
 import { createRouter, createWebHistory } from 'vue-router'
-
+import Home from '../components/BasedDesign.vue'
+import About from '../components/About.vue'
 
 
 const routes = [
- 
- 
+    {
+        path: '/',
+        component:Home
+    },
+    {
+        path: '/about',
+        component:About
+    },
 
 
     
