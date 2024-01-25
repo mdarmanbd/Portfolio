9bccfe3498295e8617d1c8a98a52904fb7922d33
mdarmanbd <md.armantext@gmail.com>
2024-01-25 23:10:18 +0600

    remove the unnesseray  code

--- src/store/project.js
+++ src/store/project.js
@@ -20,50 +20,42 @@ const project = reactive({
        
     },
     projectTwo(){
-        // this.secondProject = true
-        // this.opacityActive = true
+        
         window.open('https://github.com/mdarmanbd/module_11_Assinment_1/','_blank')
     },
 
     projectThree(){
-        // this.thirdProject = true
-        // this.opacityActive = true
+        
         window.open('https://github.com/mdarmanbd/module_1_google_weather_app','_blank')
     },
     projectFour(){
-        // this.fourProject = true
-        // this.opacityActive = true
+        
         window.open('https://github.com/mdarmanbd/ninja-js','_blank')
     },
 
     projectFive(){
-        // this.fiveProject = true
-        // this.opacityActive = true
+        
         window.open('https://cosmic-cactus-88c529.netlify.app/','_blank')
     },
 
     projectSix(){
-        // this.sixProject = true
-        // this.opacityActive = true
+        
         window.open('https://github.com/mdarmanbd/rick-n-morty','_blank')
     },
 
     projectSeven(){
-        // this.sevenProject = true
-        // this.opacityActive = true
+        
         window.open('https://mdarmanbd.github.io/e-commerce-shoes-/','_blank')
     },
 
     projectEight(){
-        // this.eightProject = true
-        // this.opacityActive = true
+        
        
         window.open('https://mdarmanbd.github.io/e-commerce_furniture_projec/','_blank')
     },
 
     projectNine(){
-        // this.nineProject = true
-        // this.opacityActive = true
+        
         
         window.open('https://mdarmanbd.github.io/e-commerce_fashion_project/#','_blank')
     },
